4654c6a2081beb819c5d9e5725fcc3e0645ae5ff
vahid bakhtiary <vahidbakhtiary@hotmail.com>
2022-02-24 09:50:17 +0330

    first commit

---
+++ Project1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1
+{
+
+   
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            int varInt = 10;
+            float varFloat = 10.5f;
+            bool varBool = true;
+            char varChar = 'A';
+            string varStr = "ALI";
+
+        }
+
+        private void BtnSum_Click(object sender, EventArgs e)
+        {
+            int num1 = 5;
+            int num2 = 10;
+            int sum = num1 + num2;
+
+            // Console.WriteLine(sum);
+            MessageBox.Show(sum.ToString());
+        }
+    }
+}
